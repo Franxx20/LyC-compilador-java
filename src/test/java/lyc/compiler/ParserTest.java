0dd5a73a3fbab33effcd0b693b0fa2031ee1f3e2
Agustin Gonzalez <agustin.elias.1000@gmail.com>
2025-05-30 00:02:21 -0300

    Modified ParserTest.Java to ignore unnecesary tests.

--- src/test/java/lyc/compiler/ParserTest.java
+++ src/test/java/lyc/compiler/ParserTest.java
@@ -3,6 +3,7 @@ package lyc.compiler;
 import java_cup.runtime.Symbol;
 import lyc.compiler.factories.ParserFactory;
 import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
@@ -22,76 +23,89 @@ public class ParserTest {
         compilationSuccessful(readFromFile("test.txt"));
     }
 
+    @Disabled
     @Test
     public void reorderTest() throws Exception {
         compilationSuccessful(readFromFile("reorder.txt"));
     }
 
+    @Disabled
     @Test
     public void negativeCalculationTest() throws Exception {
         compilationSuccessful(readFromFile("negativeCalculation.txt"));
     }
 
+    @Disabled
     @Test
     public void assignmentWithExpression() throws Exception {
         compilationSuccessful("c=d*(e-21)/4");
     }
 
+    @Disabled
     @Test
     public void syntaxError() {
         compilationError("1234");
     }
 
+    @Disabled
     @Test
     void assignments() throws Exception {
         compilationSuccessful(readFromFile("assignments.txt"));
     }
 
+    @Disabled
     @Test
     void write() throws Exception {
         compilationSuccessful(readFromFile("write.txt"));
     }
 
+    @Disabled
     @Test
     void read() throws Exception {
         compilationSuccessful(readFromFile("read.txt"));
     }
 
+    @Disabled
     @Test
     void comment() throws Exception {
         compilationSuccessful(readFromFile("comment.txt"));
     }
 
+    @Disabled
     @Test
     void init() throws Exception {
         compilationSuccessful(readFromFile("init.txt"));
     }
 
+    @Disabled
     @Test
     void and() throws Exception {
         compilationSuccessful(readFromFile("and.txt"));
     }
 
+    @Disabled
     @Test
     void or() throws Exception {
         compilationSuccessful(readFromFile("or.txt"));
     }
 
+    @Disabled
     @Test
     void not() throws Exception {
         compilationSuccessful(readFromFile("not.txt"));
     }
 
+    @Disabled
     @Test
     void ifStatement() throws Exception {
         compilationSuccessful(readFromFile("if.txt"));
     }
 
+    @Disabled
     @Test
     void whileStatement() throws Exception {
         compilationSuccessful(readFromFile("while.txt"));
     }
-
 
     private void compilationSuccessful(String input) throws Exception {
         assertThat(scan(input).sym).isEqualTo(ParserSym.EOF);
