31b91633011005b1c5e2efd8f4ddf32bf03329cb
Agustin Gonzalez <agustin.elias.1000@gmail.com>
2025-06-01 17:00:41 -0300

    Added new error type.

---
+++ src/main/java/lyc/compiler/model/UndefinedVariableException.java
@@ -0,0 +1,7 @@
+package lyc.compiler.model;
+
+public class UndefinedVariableException extends CompilerException {
+    public UndefinedVariableException(String message) {
+        super(message);
+    }
+}
