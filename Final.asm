89185c6599018d4ea6e930c0f6e5005ec26845c8
Agustin Gonzalez <agustin.elias.1000@gmail.com>
2025-06-28 20:11:47 -0300

    Update Final.asm

--- Final.asm
+++ Final.asm
@@ -11,16 +11,16 @@ MAXTEXTSIZE equ 50
 	@reOrderX4	dd	?
 	c	dd	?
 	@reOrderX3	dd	?
-	_5	dd	5
+	_5	dd	5.0
 	b	dd	?
 	@reOrderX2	dd	?
-	_4	dd	4
+	_4	dd	4.0
 	a	dd	?
 	@reOrderX1	dd	?
-	_3	dd	3
-	_2	dd	2
-	_1	dd	1
-	_0	dd	0
+	_3	dd	3.0
+	_2	dd	2.0
+	_1	dd	1.0
+	_0	dd	0.0
 	@total	dd	?
 	_2_3	dd	2.3
 	_99_	dd	99.
@@ -47,7 +47,7 @@ MAXTEXTSIZE equ 50
 	@count	dd	?
 	g	dd	?
 	@reOrderX7	dd	?
-	_9	dd	9
+	_9	dd	9.0
 	f	dd	?
 	@reOrderX6	dd	?
 	e	dd	?
@@ -56,26 +56,34 @@ MAXTEXTSIZE equ 50
 .CODE
 
 start:
-	MOV EAX,@DATA
-	MOV DS,EAX
-	MOV ES,EAX
+	MOV AX,@DATA
+	MOV DS,AX
+	MOV ES,AX
 
-	FILD	_1
+	FLD	_1
 	FSTP	a
-	FILD	_1
-	FILD	_4
+	FLD	_1
+	FLD	_4
 	FADD
 	FSTP	b
-	FILD	_2
-	FILD	_5
+	FLD	_2
+	FLD	_5
 	FMUL
-	FILD	b
+	FLD	b
 	FADD
 	FSTP	c
-	FILD	a
-	FILD	c
+	FLD	a
+	FLD	c
 	FADD
 	FSTP	d
+	DisplayFloat	a	, 2
+	newline 1
+	DisplayFloat	b	, 2
+	newline 1
+	DisplayFloat	c	, 2
+	newline 1
+	DisplayFloat	d	, 2
+	newline 1
 	FLD	_menosUno
 	FLD	_99999_99
 	FMUL
@@ -93,142 +101,149 @@ start:
 	MOV	SI,	OFFSET	_stringConstant1
 	MOV	SI,	OFFSET	var
 	STRCPY
-	FILD	a
-	FILD	b
+	FLD	a
+	FLD	b
 	FXCH
 	FCOMP
 	FSTSW	AX
 	SAHF
 	FFREE
-	JBE	THEN31
-	JMP	END_IF33
-THEN31:
+	JBE	THEN35
+	JMP	END_IF37
+THEN35:
 	DisplayString	_stringConstant2
 	newline 1
-END_IF33:
-	FILD	a
-	FILD	b
+END_IF37:
+	FLD	a
+	FLD	b
 	FXCH
 	FCOMP
 	FSTSW	AX
 	SAHF
 	FFREE
-	JBE	LAZY_AND43
-	FILD	c
-	FILD	b
+	JBE	LAZY_AND47
+	FLD	c
+	FLD	b
 	FXCH
 	FCOMP
 	FSTSW	AX
 	SAHF
 	FFREE
-	JBE	LAZY_AND43
-	JMP	THEN45
-LAZY_AND43:
-	JMP	END_IF47
-THEN45:
+	JBE	LAZY_AND47
+	JMP	THEN49
+LAZY_AND47:
+	JMP	END_IF51
+THEN49:
 	DisplayString	_stringConstant3
 	newline 1
-END_IF47:
-	FILD	a
-	FILD	b
+END_IF51:
+	FLD	a
+	FLD	b
 	FXCH
 	FCOMP
 	FSTSW	AX
 	SAHF
 	FFREE
-	JBE	OR53
-	JMP	THEN58
-OR53:
-	FILD	c
-	FILD	b
+	JBE	OR57
+	JMP	THEN62
+OR57:
+	FLD	c
+	FLD	b
 	FXCH
 	FCOMP
 	FSTSW	AX
 	SAHF
 	FFREE
-	JBE	END_IF60
-THEN58:
+	JBE	END_IF64
+THEN62:
 	DisplayString	_stringConstant4
 	newline 1
-END_IF60:
-	FILD	a
-	FILD	b
+END_IF64:
+	FLD	a
+	FLD	b
 	FXCH
 	FCOMP
 	FSTSW	AX
 	SAHF
 	FFREE
-	JBE	IF_ELSE66
+	JBE	IF_ELSE70
 	DisplayString	_stringConstant5
 	newline 1
-IF_ELSE66:
-	JMP	END_IF69
+IF_ELSE70:
+	JMP	END_IF73
 	DisplayString	_stringConstant6
 	newline 1
-END_IF69:
-	GetInteger	a
-	GetInteger	b
-	GetInteger	c
-WHILE73:
-	FILD	a
-	FILD	b
+END_IF73:
+	GetFloat	a
+	GetFloat	b
+	GetFloat	c
+WHILE77:
+	FLD	a
+	FLD	b
 	FXCH
 	FCOMP
 	FSTSW	AX
 	SAHF
 	FFREE
-	JBE	END_WHILE84
+	JBE	END_WHILE88
 	DisplayString	_stringConstant7
 	newline 1
-	FILD	a
-	FILD	_1
-	FADD
+	FLD	a
+	FLD	_1
+	FSUB
 	FSTP	a
-	JMP	WHILE73
-END_WHILE84:
+	JMP	WHILE77
+END_WHILE88:
 	MOV	SI,	OFFSET	_stringConstant8
 	MOV	SI,	OFFSET	var
 	STRCPY
 	MOV	SI,	OFFSET	var
 	MOV	SI,	OFFSET	foo
 	STRCPY
-	FILD	a
-	FILD	_3
+	FLD	a
+	FLD	_3
 	FADD
 	FSTP	@reOrderX1
-	FILD	_1
-	FILD	_1
+	FLD	_1
+	FLD	_1
 	FADD
 	FSTP	@reOrderX2
-	FILD	_9
-	FILD	b
+	FLD	_9
+	FLD	b
 	FSUB
 	FSTP	@reOrderX3
 	DisplayFloat	@reOrderX3	, 2
+	newLine 1
 	DisplayFloat	@reOrderX2	, 2
+	newLine 1
 	DisplayFloat	@reOrderX1	, 2
-	FILD	a
-	FILD	b
+	newLine 1
+	FLD	a
+	FLD	b
 	FMUL
-	FILD	_2
+	FLD	_2
 	FSUB
 	FSTP	@reOrderX4
-	FILD	c
-	FILD	_3
+	FLD	c
+	FLD	_3
 	FADD
 	FSTP	@reOrderX5
-	FILD	_1
-	FILD	_1
+	FLD	_1
+	FLD	_1
 	FADD
 	FSTP	@reOrderX6
-	FILD	_9
-	FILD	d
+	FLD	_9
+	FLD	d
 	FSUB
 	FSTP	@reOrderX7
 	DisplayFloat	@reOrderX4	, 2
+	newLine 1
 	DisplayFloat	@reOrderX5	, 2
+	newLine 1
 	DisplayFloat	@reOrderX7	, 2
+	newLine 1
 	DisplayFloat	@reOrderX6	, 2
+	newLine 1
 	FLD	_0
 	FSTP	@count
 	FLD	_0
@@ -246,7 +261,7 @@ END_WHILE84:
 	FSTSW	AX
 	SAHF
 	FFREE
-	JAE	NEG_FUNC_END_IF144
+	JAE	NEG_FUNC_END_IF148
 	FLD	@count
 	FLDZ
 	FXCH
@@ -254,14 +269,14 @@ END_WHILE84:
 	FSTSW	AX
 	SAHF
 	FFREE
-	JNE	NEG_FUNC_ELSE136
+	JNE	NEG_FUNC_ELSE140
 	FLD	_1
 	FSTP	@parBit
-	JMP	NEG_FUNC_END_IF138
-NEG_FUNC_ELSE136:
+	JMP	NEG_FUNC_END_IF142
+NEG_FUNC_ELSE140:
 	FLD	_0
 	FSTP	@parBit
-NEG_FUNC_END_IF138:
+NEG_FUNC_END_IF142:
 	FLD	@parBit
 	FSTP	@count
 	FLD	@sum
@@ -272,7 +287,7 @@ NEG_FUNC_END_IF138:
 	FLD	@elem
 	FMUL
 	FSTP	@mult
-NEG_FUNC_END_IF144:
+NEG_FUNC_END_IF148:
 	FLD	_2_3
 	FSTP	@elem
 	FLD	@elem
@@ -282,7 +297,7 @@ NEG_FUNC_END_IF144:
 	FSTSW	AX
 	SAHF
 	FFREE
-	JAE	NEG_FUNC_END_IF161
+	JAE	NEG_FUNC_END_IF165
 	FLD	@count
 	FLDZ
 	FXCH
@@ -290,14 +305,14 @@ NEG_FUNC_END_IF144:
 	FSTSW	AX
 	SAHF
 	FFREE
-	JNE	NEG_FUNC_ELSE154
+	JNE	NEG_FUNC_ELSE158
 	FLD	_1
 	FSTP	@parBit
-	JMP	NEG_FUNC_END_IF155
+	JMP	NEG_FUNC_END_IF159
 	FLD	_0
 	FSTP	@parBit
-NEG_FUNC_ELSE154:
-NEG_FUNC_END_IF155:
+NEG_FUNC_ELSE158:
+NEG_FUNC_END_IF159:
 	FLD	@parBit
 	FSTP	@count
 	FLD	@sum
@@ -308,7 +323,7 @@ NEG_FUNC_END_IF155:
 	FLD	@elem
 	FMUL
 	FSTP	@mult
-NEG_FUNC_END_IF161:
+NEG_FUNC_END_IF165:
 	FLD	f
 	FSTP	@elem
 	FLD	@elem
@@ -318,7 +333,7 @@ NEG_FUNC_END_IF161:
 	FSTSW	AX
 	SAHF
 	FFREE
-	JAE	NEG_FUNC_END_IF178
+	JAE	NEG_FUNC_END_IF182
 	FLD	@count
 	FLDZ
 	FXCH
@@ -326,14 +341,14 @@ NEG_FUNC_END_IF161:
 	FSTSW	AX
 	SAHF
 	FFREE
-	JNE	NEG_FUNC_ELSE171
+	JNE	NEG_FUNC_ELSE175
 	FLD	_1
 	FSTP	@parBit
-	JMP	NEG_FUNC_END_IF172
+	JMP	NEG_FUNC_END_IF176
 	FLD	_0
 	FSTP	@parBit
-NEG_FUNC_ELSE171:
-NEG_FUNC_END_IF172:
+NEG_FUNC_ELSE175:
+NEG_FUNC_END_IF176:
 	FLD	@parBit
 	FSTP	@count
 	FLD	@sum
@@ -344,7 +359,7 @@ NEG_FUNC_END_IF172:
 	FLD	@elem
 	FMUL
 	FSTP	@mult
-NEG_FUNC_END_IF178:
+NEG_FUNC_END_IF182:
 	FLD	_5_6
 	FSTP	@elem
 	FLD	@elem
@@ -354,7 +369,7 @@ NEG_FUNC_END_IF178:
 	FSTSW	AX
 	SAHF
 	FFREE
-	JAE	NEG_FUNC_END_IF195
+	JAE	NEG_FUNC_END_IF199
 	FLD	@count
 	FLDZ
 	FXCH
@@ -362,14 +377,14 @@ NEG_FUNC_END_IF178:
 	FSTSW	AX
 	SAHF
 	FFREE
-	JNE	NEG_FUNC_ELSE188
+	JNE	NEG_FUNC_ELSE192
 	FLD	_1
 	FSTP	@parBit
-	JMP	NEG_FUNC_END_IF189
+	JMP	NEG_FUNC_END_IF193
 	FLD	_0
 	FSTP	@parBit
-NEG_FUNC_ELSE188:
-NEG_FUNC_END_IF189:
+NEG_FUNC_ELSE192:
+NEG_FUNC_END_IF193:
 	FLD	@parBit
 	FSTP	@count
 	FLD	@sum
@@ -380,7 +395,7 @@ NEG_FUNC_END_IF189:
 	FLD	@elem
 	FMUL
 	FSTP	@mult
-NEG_FUNC_END_IF195:
+NEG_FUNC_END_IF199:
 	FLD	@count
 	FLDZ
 	FXCH
@@ -388,14 +403,14 @@ NEG_FUNC_END_IF195:
 	FSTSW	AX
 	SAHF
 	FFREE
-	JNE	NEG_FUNC_ELSE200
+	JNE	NEG_FUNC_ELSE204
 	FLD	@sum
 	FSTP	@total
-	JMP	NEG_FUNC_END_IF202
-NEG_FUNC_ELSE200:
+	JMP	NEG_FUNC_END_IF206
+NEG_FUNC_ELSE204:
 	FLD	@mult
 	FSTP	@total
-NEG_FUNC_END_IF202:
+NEG_FUNC_END_IF206:
 	FLD	@total
 	FSTP	g
 	MOV EAX, 4C00h
